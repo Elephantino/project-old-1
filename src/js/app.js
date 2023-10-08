ebd22f731d961c6768f60d7854e19d18756966bc
Рыбинский Игорь <rybinsky.igor@mail.ru>
2023-10-08 14:08:30 +0800

    commit

--- src/js/app.js
+++ src/js/app.js
@@ -6,3 +6,38 @@ import Swiper from 'swiper';
 import { Navigation, Pagination } from 'swiper/modules';
 
 const swiper = new Swiper();
+
+
+
+
+
+
+
+let svgBoxOne = document.querySelector(".svgBoxOne");
+
+svgBoxOne.addEventListener('click', function () {
+  svgBoxOne.classList.toggle('svgAnimOne');
+});
+
+
+let svgBoxTwo = document.querySelector(".svgBoxTwo");
+
+svgBoxTwo.addEventListener('click', function () {
+  svgBoxTwo.classList.toggle('svgAnimTwo');
+});
+
+
+let svgBoxThree = document.querySelector(".svgBoxThree");
+
+svgBoxThree.addEventListener('click', function () {
+  svgBoxThree.classList.toggle('svgAnimThree');
+});
+
+
+
+let svgBoxFour = document.querySelector(".svgBoxFour");
+
+svgBoxFour.addEventListener('click', function () {
+  svgBoxFour.classList.toggle('svgAnimFour');
+});
+
